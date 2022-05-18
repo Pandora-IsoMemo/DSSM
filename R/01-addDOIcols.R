7ed19a3129f1fcc4981fb22ebc6029d58c3c95c0
Jian Roachell <jianyin.roachell@inwt-statistics.de>
2022-05-18 15:42:44 +0200

    testing and optimization

--- R/01-addDOIcols.R
+++ R/01-addDOIcols.R
@@ -1,4 +1,6 @@
 generateDOI <- function(DF,citationtype,citationstyle){
+  # if there are missing values, then skip the row. and the end return the same index.
+  #library(rcrossref)
   databaseDOIout <- character()
   for (x in DF$databaseDOI) {
     databaseDOIout <- c(databaseDOIout , cr_cn(doi=x, format =  citationtype,style = citationstyle))
@@ -10,12 +12,17 @@ generateDOI <- function(DF,citationtype,citationstyle){
   compilationDOIout <- character()
   for (x in DF$compilationDOI) {
     compilationDOIout <- c(compilationDOIout , cr_cn(doi=x, format =  citationtype,style = citationstyle))
-  }
+
   DOI <- cbind(databaseDOIout, originalDOIout,compilationDOIout)
   DF <- cbind(DF,DOI)
-  return(DF)
+  return(databaseDOIout)
 }
+
+DF = test_df[1:10,]
 
 # notes:
 # 03-dataExplorer.R: line 441,
-DF = generateDOI(test_df[0:10,],"bibtex","APA") # ran this: only ten rows, the API took about 25 seconds, so 200
+a <- Sys.time()
+generateDOI(DF,"bibtex","APA") # ran this: only ten rows, the API took about 25 seconds, so 200
+#cr_cn(doi=DF$databaseDOI, "bibtex","APA")
+Sys.time() - a
