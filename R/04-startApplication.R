d3cbf0424be0affe09cdd7892e377a5200b4de31
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-01-11 13:02:46 +0100

    option for launch.browser

--- R/04-startApplication.R
+++ R/04-startApplication.R
@@ -3,10 +3,12 @@
 #' @param port port
 #'
 #' @export
-startApplication <- function(port = 4242) {
+startApplication <- function(port = 4242,
+                             launch.browser = getOption("shiny.launch.browser", interactive())) {
   runApp(
     system.file("app", package = "MpiIsoApp"),
     port = port,
-    host = "0.0.0.0"
+    host = "0.0.0.0",
+    launch.browser = launch.browser
   )
 }
