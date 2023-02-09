ec3bd8eaa6c7d75b7f5bce194bceb687787f3f4d
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-02-09 13:40:21 +0100

    clean up code

--- R/01-importModel.R
+++ R/01-importModel.R
@@ -36,15 +36,3 @@ validateImport <- function(data, showModal = FALSE){
 
   length(warnings) == 0
 }
-convertNumeric <- function(data){
-  suppressWarnings(data.num <- as.data.frame(lapply(1:ncol(data), function(x){
-    y <- as.numeric(data[,x])
-    if(sum(is.na(y)) == sum(is.na(data[,x]))){
-      return(y)
-    } else {
-      return(data[,x])
-    }
-  } )))
-  names(data.num) <- names(data)
-  data.num
-}
