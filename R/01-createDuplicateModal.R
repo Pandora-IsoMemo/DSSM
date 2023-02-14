5ca3ca20f80b04ccba0208d13a7626307d162da6
Lukas Fuchs <86153843+f-lukas@users.noreply.github.com>
2023-02-14 16:38:37 +0100

    Update R/01-createDuplicateModal.R
    Co-authored-by: Antonia Runge <antonia.runge@inwt-statistics.de>

--- R/01-createDuplicateModal.R
+++ R/01-createDuplicateModal.R
@@ -25,7 +25,7 @@ createDuplicateModal <- function(vars, session) {
                 For this reason, you can specify a rounding factor if you have selected 'Rounded Match'.
                 A value of 2, for example, means that the values are rounded to 2 decimal places before being checked for equality.
                 A value of 0 means that the values are rounded to whole numbers.
-                You can also enter negative numbers, which will result in rounding to the power of ten, e.g. the value -2 rounds to the nearest hundred.
+                You can also enter negative numbers, which will result in rounding to the power of ten, e.g. the value -2 rounds to the nearest hundred.<br><br>
                 For text columns you can choose between 'Case Sensitive', where a distinction is made between upper and lower case letters and
                 'Case Insensitive', where no distinction is made. Furthermore you have the option to ignore empty cells and spaces.
                 Optional you can specify a specific string. If you do so, rows that contain this string in the selected column are marked as duplicate.
