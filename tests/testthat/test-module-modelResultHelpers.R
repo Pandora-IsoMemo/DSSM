28095f1437a41c742d26ccc4a07c9d058d6071b7
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-03-31 11:23:50 +0200

    add test for getZValues

--- tests/testthat/test-module-modelResultHelpers.R
+++ tests/testthat/test-module-modelResultHelpers.R
@@ -13,3 +13,13 @@ test_that("Test extractZoomFromLongRange", {
   expect_equal(extractZoomFromLongRange(rangeLongitude = c(-10, 84), mapCentering = centering), 96)
   expect_equal(extractZoomFromLongRange(rangeLongitude = c(-60, 50), mapCentering = centering), 252)
   })
+
+
+test_that("Test getZvalues", {
+  testModel <- readRDS(testthat::test_path("averageR_testModel.rds"))
+
+  expect_equal(getZvalues(estimationType = "Mean", model = testModel, mapType = "Map", IndSelect = ""),
+               list(minInput = list(value = 6.1, min = 6.1, max = 15), maxInput = list(
+                 value = 15, min = 6.1, max = 15)))
+})
+
