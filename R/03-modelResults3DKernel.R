d63fb0500f7bab8508dbd3ddd710efeae0d883ab
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-04-24 13:28:40 +0200

    #139 reset model output when changing the data source (#141)
    * #139 reset model output when changing the data source
    
    * reset model for new data source in other modelling tabs
    
    * assignR: reset model, remove not used UI to set Coordinate Format

--- R/03-modelResults3DKernel.R
+++ R/03-modelResults3DKernel.R
@@ -517,10 +517,15 @@ modelResults3DKernel <- function(input, output, session, isoData, savedMaps, fru
       file = fileImport()
     )
 
+    # reset model
+    Model(NULL)
     data(activeData)
   })
 
   coordType <- reactive({
+    # reset model
+    Model(NULL)
+
     switch(
       input$dataSource,
       db = "decimal degrees",
@@ -542,6 +547,8 @@ modelResults3DKernel <- function(input, output, session, isoData, savedMaps, fru
     compressionLevel = 1)
 
   observe(priority = 100, {
+    # reset model
+    Model(NULL)
     ## update data ----
     data(uploadedData$data)
   }) %>%
@@ -1168,6 +1175,8 @@ modelResults3DKernel <- function(input, output, session, isoData, savedMaps, fru
 
   fileImport <- reactiveVal(NULL)
   observe({
+    # reset model
+    Model(NULL)
     if (length(importedDat()) == 0 ||  is.null(importedDat()[[1]])) fileImport(NULL)
 
     req(length(importedDat()) > 0, !is.null(importedDat()[[1]]))
