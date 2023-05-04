17a81ab7d6ecde71605f06a864249759dcb3da94
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-05-04 11:36:29 +0200

    use maps without borders or names, add new maps

--- R/02-leafletSettings.R
+++ R/02-leafletSettings.R
@@ -7,25 +7,40 @@ leafletSettingsUI <- function(id, title = "") {
 
   tagList(
     tags$h2(title),
-    selectInput(
+    selectizeInput(
       ns("LeafletType"),
-      "Map type",
-      choices = c(
+      label = "Map type",
+      choices = list(
+        `borders & names`= c(
         "CartoDB Positron" = "CartoDB.Positron",
-        "CartoDB Positron No Labels" = "CartoDB.PositronNoLabels",
         "OpenStreetMap Mapnik" = "OpenStreetMap.Mapnik",
         "OpenStreetMap DE" = "OpenStreetMap.DE",
         "OpenTopoMap" = "OpenTopoMap",
         "Stamen TonerLite" = "Stamen.TonerLite",
         "Esri" = "Esri",
         "Esri WorldTopoMap" = "Esri.WorldTopoMap",
+        "Esri OceanBasemap" = "Esri.OceanBasemap"
+      ),
+      `only borders`= c(
+        "CartoDB Positron No Labels" = "CartoDB.PositronNoLabels"
+      ),
+      `plain maps`= c(
         "Esri WorldImagery" = "Esri.WorldImagery",
         "Esri WorldTerrain" = "Esri.WorldTerrain",
         "Esri WorldShadedRelief" = "Esri.WorldShadedRelief",
-        "Esri WorldPhysical" = "Esri.WorldPhysical",
-        "Esri OceanBasemap" = "Esri.OceanBasemap"
+        "Esri WorldPhysical" = "Esri.WorldPhysical"
+      ),
+      `custom maps` = c(
+        "Stamen.Watercolor" = "Stamen.Watercolor"
       )
     ),
+    options = list(create = TRUE)
+    ),
+    helpText(HTML(paste0("Find more maps at: <br>",
+                         tags$a(href = "https://leaflet-extras.github.io/leaflet-providers/preview/",
+                                "https://leaflet-extras.github.io/leaflet-providers/preview/",
+                                target = "_blank"),
+                         "<br> Type the name and click 'Add...'."))),
     fluidRow(column(6, checkboxInput(
       ns("includeNorthArrow"), "North Arrow"
     )),
