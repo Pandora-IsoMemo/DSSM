9536129dcb9813f500b03bac8a7ba79a3c6f4926
Antonia Runge <antonia.runge@inwt-statistics.de>
2024-05-03 10:25:52 +0200

    new test for binaryMap

---
+++ R/01-mapOperations.R
@@ -0,0 +1,14 @@
+#' Get Binary Predictions
+#'
+#' @param XPred1 prediction matrix of first map
+#' @param XPred2 prediction matrix of second map
+#' @return prediction matrix of resulting map
+getBinaryPredictions <- function(XPred1, XPred2) {
+  # your code doing the calculations
+
+  # this is just a placeholder!
+  result <- XPred1
+
+  # return the result
+  return(result)
+}
