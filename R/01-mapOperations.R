ee5bc055020043c53aa8d625b5eaf1c6aa4a7bc7
Antonia Runge <antonia.runge@inwt-statistics.de>
2024-05-03 10:44:15 +0200

    update docu

--- R/01-mapOperations.R
+++ R/01-mapOperations.R
@@ -1,8 +1,8 @@
 #' Get Binary Predictions
 #'
-#' @param XPred1 prediction matrix of first map
-#' @param XPred2 prediction matrix of second map
-#' @return prediction matrix of resulting map
+#' @param XPred1 (data.frame) prediction matrix of first map
+#' @param XPred2 (data.frame) prediction matrix of second map
+#' @return (data.frame) prediction matrix of resulting map
 getBinaryPredictions <- function(XPred1, XPred2) {
   # your code doing the calculations
 
