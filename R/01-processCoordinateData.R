1e1e62b918ad60521a1f3b4d0e1e5262c1646125
Antonia Runge <antonia.runge@inwt-statistics.de>
2024-05-08 10:29:21 +0200

    more docu

--- R/01-processCoordinateData.R
+++ R/01-processCoordinateData.R
@@ -113,7 +113,8 @@ removeDataOutsideRestriction <- function(data, Latitude, Longitude, restriction)
 #' Center Data
 #'
 #' Transfer data to the center of the map, either Europe or Pacific by adding or substracting 360°
-#'  from the Longitude
+#' from the Longitude. If "Europe" longitudes are transposed to the range (-180, 180),  if
+#' "Pacific" longitudes are transposed to the range (0, 360).
 #'
 #' @param data (data.frame) data containing column "Longitude"
 #' @param center (character) center to shift data to, either "Europe" or "Pacific"
