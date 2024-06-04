ecf623ad7ce4430d29bb4aebd7d23be7c01ca8d0
Antonia Runge <antonia.runge@inwt-statistics.de>
2024-06-04 09:37:17 +0200

    update docu and vignettes

---
+++ vignettes/process_coordinate_data.R
@@ -0,0 +1,24 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup, include = FALSE---------------------------------------------------
+library(MpiIsoApp)
+
+## ----echo=FALSE, fig.cap="Center of Data", out.width='650px'------------------
+knitr::include_graphics("../man/figures/centerOfData.png")
+
+## ----echo=FALSE, fig.cap="Warning for Centering missmatch", out.width='650px'----
+knitr::include_graphics("../man/figures/centerOfDataWarning.png")
+
+## ----echo=FALSE, fig.cap="Border correction", out.width='650px'---------------
+knitr::include_graphics("../man/figures/borderCorrection.png")
+
+## ----echo=FALSE, fig.cap="Border correction idea", out.width='650px'----------
+knitr::include_graphics("../man/figures/borderCorrectionLogic.png")
+
+## ----echo=FALSE, fig.cap="Map centering", out.width='650px'-------------------
+knitr::include_graphics("../man/figures/mapCentering.png")
+
