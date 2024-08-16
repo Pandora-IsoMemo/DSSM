0aa1bb86ca6a3669032fa8a42af4d1ce5fbbc22a
Lukas Fuchs <86153843+f-lukas@users.noreply.github.com>
2024-08-16 09:09:21 +0200

    fix cluster ids (#239)
    * fix cluster ids
    
    * extract logic into function
    
    * fix name of df
    
    ---------
    
    Co-authored-by: Antonia Runge <antonia.runge@inwt-statistics.de>

--- R/01-predictNearestCluster.R
+++ R/01-predictNearestCluster.R
@@ -5,3 +5,18 @@ predictNearestCluster <- function(object, newdata){
   dist_mat <- dist_mat[-seq(n_centers), seq(n_centers)]
   max.col(-dist_mat)
 }
+
+#' Make cluster ids continuous
+#'
+#' When Mclust is used with e.g. 10 clusters, it can still happen that some clusters are empty
+#' In this case we would see a jump in cluster ids e.g. 1,2,5,...
+#' To prevent this, we change the cluster ids in the last step.
+#'
+#' @param column_with_ids A vector with cluster ids
+makeClusterIdsContinuous <- function(column_with_ids) {
+  if (length(column_with_ids) == 0) {
+    return(column_with_ids)
+  }
+
+  match(column_with_ids, sort(unique(column_with_ids)))
+}
