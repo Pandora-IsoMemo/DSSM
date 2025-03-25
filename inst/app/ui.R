ef5ef8ec9b10093aaa55a8f8e6a5006eb28cacac
Antonia Runge <antonia.runge@inwt-statistics.de>
2025-03-25 08:48:03 +0100

    update header

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -38,34 +38,6 @@ tagList(
       tags$script(src = "ReSources/removeName.js")
     ) else NULL
   ),
-  div(
-    id = "header-right",
-    div(
-      id = "logo-mpi",
-      tags$a(href = "https://www.mpg.de/en",
-             img(src = "IsoMemo/MPIlogo.png", alt = "Supported by the Max Planck society"),
-             target = "_blank"
-             )
-    ),
-    div(
-      id = "logo-isomemo",
-      tags$a(href = "https://isomemo.com/",
-             img(src = "IsoMemo/IsoMemoLogo.png", alt = "IsoMemo"),
-             target = "_blank"
-             )
-    ),
-    div(
-      id = "further-help",
-      tags$button(
-        onclick = "window.open('https://isomemo.com','_blank');",
-        class = "btn btn-default",
-        "Further Help"
-      )
-    ),
-    div(
-      id = "help",
-      actionButton("getHelp", "?")
-    )
-  ),
+  shinyTools::headerButtonsUI(id = "header", help_link = "https://pandora-isomemo.github.io/DSSM/"),
   shinyjs::useShinyjs()
 )
